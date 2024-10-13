01113e520b3e92b4b9c4817f14a292163f71ca86
Đỗ Hữu Ngọc Tuấn <139521082+dohuungoctuan@users.noreply.github.com>
2024-10-13 15:50:24 +0700

    Bài 37. Dãy con dài nhất.cpp

--- Mảng 1 chiều nâng cao/[Mảng 1 Chiều Nâng Cao]. Bài 37. Dãy con dài nhất/Bài 37. Dãy con dài nhất.cpp
+++ Mảng 1 chiều nâng cao/[Mảng 1 Chiều Nâng Cao]. Bài 37. Dãy con dài nhất/Bài 37. Dãy con dài nhất.cpp
@@ -15,9 +15,9 @@ int main()
         prefix[i+1] = prefix[i] + a[i];
     }
     int maxLength = 0;
-    for(int i=0;i<n-maxLength;i++)
-    {
-        for(int j=i+maxLength;j<n;j++)
+    for(int i=0;i<n-maxLength;i++) // (i < n-maxLength) vì trong khoảng từ n-maxLength đến n độ dài tối đa chỉ bảng maxLength 
+    {                              // nên cho dù có dãy con thỏa mãn cũng không giải quyết được gì.
+        for(int j=i+maxLength;j<n;j++)   // (j = i+maxLength) vì từ i -> maxLength dù có dãy con thỏa mãn nhưng độ dài tối đa cũng chỉ bằng maxLength.
         {
             if((prefix[j+1] - prefix[i]) % k == 0){
                 int length = j - i + 1;
